a7a82ef1515188a7cecfbcb567850411d12fedf8
23DP3DSkor <166013323+23DP3DSkor@users.noreply.github.com>
2024-09-06 08:19:11 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,34 @@
 package lv.rvt;
 
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your first Hello World app!
+        // String name = "Dominik";
+        // int age = 17;
+        // System.out.println(age);
+        // System.out.println(name);
 
+
+    //     String userName = "Dominik";
+    //     int userAge = 17;
+    //     Boolean userPassword = true;
+
+    //     Scanner scanner = new Scanner(System.in);
+    //     String name = scanner.nextLine();
+    //     System.out.println(name);
+
+            
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ievadi savu vardi");
+        String name = scanner.nextLine();
+        System.out.println("Ievadi savu uzvards");
+        String lastName = scanner.nextLine();
+        System.out.println("Ievadi savu grupu");
+        String group = scanner.nextLine();
+        System.out.println(name + lastName + group);
     }
-
 }
