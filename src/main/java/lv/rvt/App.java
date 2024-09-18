5ca19d1e7b57d92fd10cc175c46d84758e80c0f1
23DP3DSkor <166013323+23DP3DSkor@users.noreply.github.com>
2024-09-18 06:59:37 +0000

    Simple Calculator

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -21,14 +21,37 @@ public class App
     //     System.out.println(name);
 
             
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("Ievadi savu vardi");
+        // String name = scanner.nextLine();
+        // System.out.println("Ievadi savu uzvards");
+        // String lastName = scanner.nextLine();
+        // System.out.println("Ievadi savu grupu");
+        // String group = scanner.nextLine();
+        // System.out.println(name + lastName + group);
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Ievadi savu vardi");
-        String name = scanner.nextLine();
-        System.out.println("Ievadi savu uzvards");
-        String lastName = scanner.nextLine();
-        System.out.println("Ievadi savu grupu");
-        String group = scanner.nextLine();
-        System.out.println(name + lastName + group);
+        System.out.println("Ievadiet pirmo skaitli: ");
+        int number1 = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadiet otro skaitli: ");
+        int number2 = Integer.valueOf(scanner.nextLine());
+        int summa = number1 + number2;
+        int starp = number1 - number2;
+        int reiz = number1 * number2;
+        double videj = (number1 + number2) / 2;
+
+        System.out.println(number1 + " + " + number2 + " = " + summa);
+        System.out.println(number1 + " - " + number2 + " = " + starp);
+        System.out.println(number1 + " * " +  number2 + "=" + reiz);
+        System.out.println("(" + number1 + "+" +number2 + ") / 2 = " + videj);
+
+
+        // System.out.println("four: " + 4);
+        // int first = 3;
+        // int second = 2;
+        // double result = first / second;
+        // System.out.println(result);
     }
 }
