3dd7a918847694ecd4b2ec31c15abe7bd0066821
23DP3DSkor <166013323+23DP3DSkor@users.noreply.github.com>
2024-09-20 08:03:28 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -58,38 +58,38 @@ public class App
         //         if (true) {
         //             System.out.println("This code is unavoidable!");
         // }
-        // Scanner scanner = new Scanner(System.in);
-        // System.out.println("Give speed: ");
-        // int speed = Integer.valueOf(scanner.nextLine());
-        // if ( speed > 120){
-        //     System.out.println("Speeding ticket!");
-        // }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give points [0-100]: ");
-        int point = Integer.valueOf(scanner.nextLine());
-        if (point < 0){
-            System.out.println("Grade: impossible!");
+        System.out.println("Give speed: ");
+        int speed = Integer.valueOf(scanner.nextLine());
+        if ( speed > 120){
+            System.out.println("Speeding ticket!");
         }
-        else if (point < 49){
-            System.out.println("Grade: failed");
-        }
-        else if (point < 59){
-            System.out.println("Grade: 1");
-        }
-        else if (point < 69){
-            System.out.println("Grade: 2");
-        }
-        else if (point < 79){
-            System.out.println("Grade: 3");
-        }
-        else if (point < 89){
-            System.out.println("Grade: 4");
-        }
-        else if (point <= 100){
-            System.out.println("Grade: 5");
-        }
-        else{
-            System.out.println("Grade: incredible!");
-        }
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Give points [0-100]: ");
+        // int point = Integer.valueOf(scanner.nextLine());
+        // if (point < 0){
+        //     System.out.println("Grade: impossible!");
+        // }
+        // else if (point < 49){
+        //     System.out.println("Grade: failed");
+        // }
+        // else if (point < 59){
+        //     System.out.println("Grade: 1");
+        // }
+        // else if (point < 69){
+        //     System.out.println("Grade: 2");
+        // }
+        // else if (point < 79){
+        //     System.out.println("Grade: 3");
+        // }
+        // else if (point < 89){
+        //     System.out.println("Grade: 4");
+        // }
+        // else if (point <= 100){
+        //     System.out.println("Grade: 5");
+        // }
+        // else{
+        //     System.out.println("Grade: incredible!");
+        // }
     }
 }
