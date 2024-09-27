53565835b4502531896cc015fda1a091b92f1594
23DP3DSkor <166013323+23DP3DSkor@users.noreply.github.com>
2024-09-27 08:09:23 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -91,26 +91,48 @@ public class App
         // else{
         //     System.out.println("Grade: incredible!");
         // }
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.println("Value of the gift?");
+        // int value = Integer.valueOf(scanner.nextLine());
+        // if (value >= 5000 && value <= 25000) {
+        //     System.out.println("Tax: " + (100 + (value-5000) * 0.08));
+        // }
+        // else if (value < 5000){
+        //     System.out.println("No tax!");
+        // }
+        // else if (value >= 25000 && value <= 55000) {
+        //     System.out.println("Tax: " + (1700 + (value-25000) * 0.10));
+        // }
+        // else if (value >= 55000 && value <= 200000) {
+        //     System.out.println("Tax: " + (4700 + (value-55000) * 0.12));
+        // }
+        // else if (value >= 200000 && value <= 1000000) {
+        //     System.out.println("Tax: " + (22100 + (value-200000) * 0.15));
+        // }
+        // else{
+        //     System.out.println("Tax: " + (142100 + (value-1000000) * 0.17));
+        // }
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Value of the gift?");
-        int value = Integer.valueOf(scanner.nextLine());
-        if (value >= 5000 && value <= 25000) {
-            System.out.println("Tax: " + (100 + (value-5000) * 0.08));
+        int result = 0;
+        int summa = 0;
+        while (true) {
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            if (number == 0) {
+                break;
+
+            }
+    
+            if (number > 0) {
+                result = result + 1;
+                summa = summa + number;
+                continue;
+            }
+            else{
+                continue;
+            }
         }
-        else if (value < 5000){
-            System.out.println("No tax!");
-        }
-        else if (value >= 25000 && value <= 55000) {
-            System.out.println("Tax: " + (1700 + (value-25000) * 0.10));
-        }
-        else if (value >= 55000 && value <= 200000) {
-            System.out.println("Tax: " + (4700 + (value-55000) * 0.12));
-        }
-        else if (value >= 200000 && value <= 1000000) {
-            System.out.println("Tax: " + (22100 + (value-200000) * 0.15));
-        }
-        else{
-            System.out.println("Tax: " + (142100 + (value-1000000) * 0.17));
-        }
+        System.out.println("Number of numbers: " + result);
+        System.out.println("Sum of the numbers: " + summa);
     }
 }
